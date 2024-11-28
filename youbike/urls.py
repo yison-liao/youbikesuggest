725ff28b5c3908ead3f463ef054854229b48fdcd
eason1221 <e.son.liao@gmail.com>
2024-11-28 23:27:53 +0800

    add url

--- youbike/urls.py
+++ youbike/urls.py
@@ -4,4 +4,5 @@ from youbike import views as youbike_views
 
 api_pattern = [
     re_path(r"^bikestationstatus", youbike_views.YoubikeStationsStatus.as_view()),
+    re_path(r"^bikestationinfo", youbike_views.YoubikeStationsInfo.as_view()),
 ]
